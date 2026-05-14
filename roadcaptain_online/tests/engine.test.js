03286dc91918f8ed6673ac133acbb8a08ef4638c
Rhegium <a.travia@gmail.com>
2026-05-14 21:26:05 +0200

    Delete roadcaptain_online directory

--- roadcaptain_online/tests/engine.test.js
+++
@@ -1,6 +0,0 @@
-import { planFuelStops, calculateAutonomyKm } from "../engine.js";
-const stops = planFuelStops({routeKm:500, pois:[{name:"A",routeKm:160},{name:"B",routeKm:320}], stopEveryKm:150, forwardWindowKm:25, maxAutonomyKm:200});
-if (stops.length !== 2 || stops[0].name !== "A") throw new Error("fuel planner failed");
-const autonomy = calculateAutonomyKm({tankCapacityL:15.5, consumptionKmL:13});
-if (Math.round(autonomy) !== 202) throw new Error("autonomy calculation failed");
-console.log("engine ok");
